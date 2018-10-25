f4b6a29a91f8647df38a89e9a0193ceea2c2b320
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2018-10-25 15:08:13 +0200

    MAINT: Moved save_mpiio into a separate I/O module.

---
+++ PyLB/IO.py
@@ -0,0 +1,80 @@
+# Copyright 2017-2018 Lars Pastewka, Andreas Greiner
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import numpy as np
+from mpi4py import MPI
+
+###
+
+
+def save_mpiio(comm, fn, g_kl):
+    """
+    Write a global two-dimensional array to a single file in the npy format
+    using MPI I/O: https://docs.scipy.org/doc/numpy/neps/npy-format.html
+
+    Arrays written with this function can be read with numpy.load.
+
+    Parameters
+    ----------
+    comm
+        MPI communicator.
+    fn : str
+        File name.
+    g_kl : array
+        Portion of the array on this MPI processes.
+    """
+    from numpy.lib.format import dtype_to_descr, magic
+    magic_str = magic(1, 0)
+
+    local_nx, local_ny = g_kl.shape
+    nx = np.empty_like(local_nx)
+    ny = np.empty_like(local_ny)
+
+    commx = comm.Sub((True, False))
+    commy = comm.Sub((False, True))
+    commx.Allreduce(np.asarray(local_nx), nx)
+    commy.Allreduce(np.asarray(local_ny), ny)
+
+    arr_dict_str = str({'descr': dtype_to_descr(g_kl.dtype),
+                        'fortran_order': False,
+                        'shape': (np.asscalar(nx), np.asscalar(ny))})
+    while (len(arr_dict_str) + len(magic_str) + 2) % 16 != 15:
+        arr_dict_str += ' '
+    arr_dict_str += '\n'
+    header_len = len(arr_dict_str) + len(magic_str) + 2
+
+    offsetx = np.zeros_like(local_nx)
+    commx.Exscan(np.asarray(ny*local_nx), offsetx)
+    offsety = np.zeros_like(local_ny)
+    commy.Exscan(np.asarray(local_ny), offsety)
+
+    file = MPI.File.Open(comm, fn, MPI.MODE_CREATE | MPI.MODE_WRONLY)
+    if MPI.COMM_WORLD.Get_rank() == 0:
+        file.Write(magic_str)
+        file.Write(np.int16(len(arr_dict_str)))
+        file.Write(arr_dict_str.encode('latin-1'))
+    mpitype = MPI._typedict[g_kl.dtype.char]
+    filetype = mpitype.Create_vector(g_kl.shape[0], g_kl.shape[1], ny)
+    filetype.Commit()
+    file.Set_view(header_len + (offsety+offsetx)*mpitype.Get_size(),
+                  filetype=filetype)
+    file.Write_all(g_kl.copy())
+    filetype.Free()
+    file.Close()
--- simulators/parallel_lid_drive_cavity/cavity_opt2.py
+++ simulators/parallel_lid_drive_cavity/cavity_opt2.py
@@ -41,6 +41,7 @@ from mpi4py import MPI
 import numpy as np
 
 import PyLB as D2Q9
+from PyLB.IO import save_mpiio
 
 ### Parameters
 
@@ -207,63 +208,6 @@ def communicate(f_ikl):
     comm.Sendrecv(f_ikl[:, :, -2].copy(), top_dst,
                   recvbuf=recvbuf, source=top_src)
     f_ikl[:, :, 0] = recvbuf
-
-### I/O functions
-
-def save_mpiio(comm, fn, g_kl):
-    """
-    Write a global two-dimensional array to a single file in the npy format
-    using MPI I/O: https://docs.scipy.org/doc/numpy/neps/npy-format.html
-
-    Arrays written with this function can be read with numpy.load.
-
-    Parameters
-    ----------
-    comm
-        MPI communicator.
-    fn : str
-        File name.
-    g_kl : array
-        Portion of the array on this MPI processes.
-    """
-    from numpy.lib.format import dtype_to_descr, magic
-    magic_str = magic(1, 0)
-
-    local_nx, local_ny = g_kl.shape
-    nx = np.empty_like(local_nx)
-    ny = np.empty_like(local_ny)
-
-    commx = comm.Sub((True, False))
-    commy = comm.Sub((False, True))
-    commx.Allreduce(np.asarray(local_nx), nx)
-    commy.Allreduce(np.asarray(local_ny), ny)
-
-    arr_dict_str = str({ 'descr': dtype_to_descr(g_kl.dtype),
-                         'fortran_order': False,
-                         'shape': (np.asscalar(nx), np.asscalar(ny)) })
-    while (len(arr_dict_str) + len(magic_str) + 2) % 16 != 15:
-        arr_dict_str += ' '
-    arr_dict_str += '\n'
-    header_len = len(arr_dict_str) + len(magic_str) + 2
-
-    offsetx = np.zeros_like(local_nx)
-    commx.Exscan(np.asarray(ny*local_nx), offsetx)
-    offsety = np.zeros_like(local_ny)
-    commy.Exscan(np.asarray(local_ny), offsety)
-
-    file = MPI.File.Open(comm, fn, MPI.MODE_CREATE | MPI.MODE_WRONLY)
-    if rank == 0:
-        file.Write(magic_str)
-        file.Write(np.int16(len(arr_dict_str)))
-        file.Write(arr_dict_str.encode('latin-1'))
-    mpitype = MPI._typedict[g_kl.dtype.char]
-    filetype = mpitype.Create_vector(g_kl.shape[0], g_kl.shape[1], ny)
-    filetype.Commit()
-    file.Set_view(header_len + (offsety+offsetx)*mpitype.Get_size(),
-                  filetype=filetype)
-    file.Write_all(g_kl.copy())
-    filetype.Free()
-    file.Close()
 
 ### Initialize MPI communicator
 
