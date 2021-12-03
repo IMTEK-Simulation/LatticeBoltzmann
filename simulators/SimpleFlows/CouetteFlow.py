a2fdb085bfca3c690c736d6817e806609cb5e731
Christoph Moser <christoph.moser@students.uni-freiburg.de>
2021-12-03 15:52:09 +0100

    - will start with the streaming part, seems to be easier on the head to be frank - as it is without optimizations ill not use np.roll otherwise ill just copy and dont understand - added bodies and tested the ploting in pycharm, kinda nice seperate window pops open :)

--- simulators/SimpleFlows/CouetteFlow.py
+++ simulators/SimpleFlows/CouetteFlow.py
@@ -7,7 +7,8 @@ some general remarks
 ill do this one here in the most explixite form imaginable, in the Poiseuille Flow ill start doing optimizations
 but as I am learning, ill do this first try the "dumb" way. I am aware that withhin the project there allready good
 implementations of basically all the operations. But for the first try I will ignore them and will use my
-implementations. (In the second try PoiseuilleFlow ill use them thou.) This should motivate my approach here.
+implementations. (In the second try PoiseuilleFlow ill use them thou.) This should motivate my approach here. Btw in
+D2Q9.
 
 '''
 '''
@@ -18,7 +19,35 @@ Variablennamen, Methodennamen, Funktionsnamen mit unter_strichen
 Bitte nicht CamelCase und Unterstriche mischen
 '''
 
-print("Couette_Flow")
+''' Imports '''
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+''' Setup '''
+print("Couetteflow")
+
+#weights and velocity sets
+weights = np.array([4/9,1/9,1/9,1/9,1/36, 1/36, 1/36,1/36])
+velocity_set = np.array([[0, 1, 0, -1, 0, 1, -1, -1, 1],
+                         [0,0,1,0,-1,1,1,-1,-1]])
+
+''' functions '''
+def equilibrium():
+    pass
+
+#start with Streaming kinda easier on the head
+def streaming():
+    pass
+
+def collision():
+    pass
+
+''' body '''
+
+
+''' visualization '''
+#quiver?!
 
 
 
