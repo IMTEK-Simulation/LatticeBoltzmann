0d1e0f696e742be9b5704c8fa9d9976b9111012f
Christoph Moser <christoph.moser@students.uni-freiburg.de>
2021-12-03 15:12:53 +0100

    - tested weather the hooks worked (in shear Wave decay) - the installation of MPI4py didnt work thou, not to important now - added files for my simulators in the dir - wrote down my thoughts

---
+++ simulators/SimpleFlows/CouetteFlow.py
@@ -0,0 +1,27 @@
+
+#Todo: remove us
+'''
+To get a basic feel here ill implement the Couette Flow between two plates
+-----
+some general remarks
+ill do this one here in the most explixite form imaginable, in the Poiseuille Flow ill start doing optimizations
+but as I am learning, ill do this first try the "dumb" way. I am aware that withhin the project there allready good
+implementations of basically all the operations. But for the first try I will ignore them and will use my
+implementations. (In the second try PoiseuilleFlow ill use them thou.) This should motivate my approach here.
+
+'''
+'''
+Remider CodingRules: 
+Zeilenumbruch bei Spalte 120
+Modulname, Klassennamen als CamelCase
+Variablennamen, Methodennamen, Funktionsnamen mit unter_strichen
+Bitte nicht CamelCase und Unterstriche mischen
+'''
+
+print("Couette_Flow")
+
+
+
+
+
+
---
+++ simulators/SimpleFlows/PoiseuilleFlow.py
@@ -0,0 +1,14 @@
+
+#Todo: remove us
+'''
+To get a basic feel for LB-Sim implement the  Poiseuille Flow between two plates
+
+
+'''
+'''
+Remider CodingRules: 
+Zeilenumbruch bei Spalte 120
+Modulname, Klassennamen als CamelCase
+Variablennamen, Methodennamen, Funktionsnamen mit unter_strichen
+Bitte nicht CamelCase und Unterstriche mischen
+'''
