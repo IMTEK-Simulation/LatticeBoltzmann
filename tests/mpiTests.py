4c980381f11850f6922c9fccbce2d2cf701bc8c6
Christoph Moser <christoph.mos.studium@gmail.com>
2022-02-14 15:00:57 +0100

    - did the monte carlo estimation - should mentiton that this is kinda hard with just 2 cores lol

--- tests/mpiTests.py
+++ tests/mpiTests.py
@@ -9,13 +9,12 @@ Test- and Playground for the mpi implementation
 '''
 # TODO
 '''
- -> monte carlo implementation get the first feel
+ -> monte carlo implementation get the first feel ;<;
  -> send and recive random stuff
     -> maybe do some unittests here not sure thou
  -> try out domain decomposition i guess  
 '''
 import unittest
-import numpy as np
 from mpi4py import MPI
 import ipyparallel as ipp
 
@@ -27,7 +26,28 @@ def mpi_example():
     return f"Hello World from rank {comm.Get_rank()}. total ranks={comm.Get_size()}"
 
 # try out the monte carlo stuff
+# basically stole out of the lecture and modified it a bit lol
+def monte_carlo():
+    import numpy as np
+    ###
+    comm = MPI.COMM_WORLD
+    size = comm.Get_size()
+    rank = comm.Get_rank()
+    ###
+    ntotal = 1000000
+    nlocal = ntotal//size
+    if rank == 0:
+        # for all the cases when it can not be divided equally
+        nlocal = ntotal - nlocal*(size-1)
+        #
+    x = np.random.random(nlocal)
+    y = np.random.random(nlocal)
 
+    nsucess = (x**2 + y**2 < 1).sum()
+
+    reduced_nsucess = np.array(0)
+    comm.Reduce(nsucess,reduced_nsucess, op= MPI.SUM, root = 0)
+    return f"{4*reduced_nsucess/ntotal}"
 
 # request an MPI cluster with 2 engines
 with ipp.Cluster(engines='mpi', n=cores
@@ -36,7 +56,7 @@ with ipp.Cluster(engines='mpi', n=cores
     # suited for MPI style computation
     view = rc.broadcast_view()
     # run the mpi_example function on all engines in parallel
-    r = view.apply_sync(mpi_example)
+    r = view.apply_sync(monte_carlo)
     # Retrieve and print the result from the engines
     print("\n".join(r))
 # at this point, the cluster processes have been shutdow
