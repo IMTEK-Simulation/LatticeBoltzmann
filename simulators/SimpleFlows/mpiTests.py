dd42931ce890f92e4ff0a8374f2cd294b3979a0e
Christoph Moser <christoph.moser@students.uni-freiburg.de>
2022-03-09 16:18:57 +0100

    made it work noice :D

--- simulators/SimpleFlows/mpiTests.py
+++ simulators/SimpleFlows/mpiTests.py
@@ -265,8 +265,8 @@ def test_4_simulation():
     process_info = slidingLidMPI.fill_mpi_struct_fields(rank, size, 2, 2, base_lenght, relaxation, steps, uw)
     # process_info.boundaries_info.apply_left = True
     # process_info.boundaries_info.apply_right = True
-    process_info.boundaries_info.apply_top = True
-    process_info.boundaries_info.apply_bottom = True
+    # process_info.boundaries_info.apply_top = True
+    # process_info.boundaries_info.apply_bottom = True
     slidingLidMPI.sliding_lid_mpi(process_info, comm)
     return f"{process_info}"
 
