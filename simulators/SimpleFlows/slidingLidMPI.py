219c2b859390e00584eb24078b1166f857f73bea
Christoph Moser <christoph.moser@students.uni-freiburg.de>
2022-03-09 16:17:43 +0100

    rollback i think the sending recv gets messed up and maybe sth gets written wrongly idk added send + recvtags

--- simulators/SimpleFlows/slidingLidMPI.py
+++ simulators/SimpleFlows/slidingLidMPI.py
@@ -229,21 +229,21 @@ def comunicate(grid,info,comm):
     # Right + Left
     if not info.boundaries_info.apply_right:
         recvbuf = grid[:, -1, :].copy()
-        comm.Sendrecv(grid[:,-2, :].copy(), info.neighbors.right, recvbuf=recvbuf)
+        comm.Sendrecv(grid[:,-2, :].copy(), info.neighbors.right, recvbuf=recvbuf, sendtag = 11, recvtag = 11)
         grid[:, -1, :] = recvbuf
     if not info.boundaries_info.apply_left:
         recvbuf = grid[:, 0, :].copy()
-        comm.Sendrecv(grid[:, 1, :].copy(), info.neighbors.left, recvbuf=recvbuf)
+        comm.Sendrecv(grid[:, 1, :].copy(), info.neighbors.left, recvbuf=recvbuf, sendtag = 11, recvtag = 11)
         grid[:, 0, :] = recvbuf
 
     # Bottom + Top
     if not info.boundaries_info.apply_bottom:
         recvbuf = grid[:,: ,0 ].copy()
-        comm.Sendrecv(grid[:, :,1 ].copy(), info.neighbors.bottom, recvbuf=recvbuf)
+        comm.Sendrecv(grid[:, :,1 ].copy(), info.neighbors.bottom, recvbuf=recvbuf, sendtag = 99, recvtag = 99)
         grid[:, :, 0] = recvbuf
     if not info.boundaries_info.apply_top:
         recvbuf = grid[:, :, -1].copy()
-        comm.Sendrecv(grid[:, :, -2].copy(), info.neighbors.top, recvbuf=recvbuf)
+        comm.Sendrecv(grid[:, :, -2].copy(), info.neighbors.top, recvbuf=recvbuf, sendtag = 99, recvtag = 99)
         grid[:, :, -1] = recvbuf
 
 
